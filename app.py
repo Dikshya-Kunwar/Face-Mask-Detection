39f925390cfb2b635346135e9af26dd6d67c10c4
Dikshya <kunwardikshya13@gmail.com>
2026-06-14 20:54:23 +0545

    added opencamera and stopcamera button

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask,render_template,Response
 import cv2
+import os
+import signal
 app=Flask(__name__)
 from detect_face_mask import edit_frames
+import warnings
+warnings.filterwarnings("ignore")
+
 
 
 def generate_frames():
-    camera =cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(0)
     while True:   
         success,frame1=camera.read()
+        print(success)
         if not success:
             break
         else:
             frame1 = edit_frames(frame1)
             ret,buffer=cv2.imencode('.jpg',frame1)
             frame = buffer.tobytes()
+
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -27,4 +34,9 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
+
+@app.route('/stop')
+def stop():
+    os.kill(os.getpid(), signal.SIGINT)
+    return "Server shutting down..."
--- detect_face_mask.py
+++ detect_face_mask.py
@@ -65,7 +65,6 @@ def edit_frames(frame):
 	frame = imutils.resize(frame, width=400)
 
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-	print(locs,preds)
 	for (box, pred) in zip(locs, preds):
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
@@ -75,20 +74,9 @@ def edit_frames(frame):
 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
 		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-		print(label)
 		cv2.putText(frame, label, (startX, startY - 10),
 			  cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 	return(frame)
 	
-
-
-		# key = cv2.waitKey(1) & 0xFF
-
-	
-		# if key == ord("q"):
-		# 	break
-
-
-# cv2.destroyAllWindows()
-# vs.stop()
+		
